43b7c50929ec247be449fe31dd7bd12ae558b4d5
Anton Rastyazhenko <rastyazhenko.anton@gmail.com>
2026-06-23 06:38:14 +0300

    Update models.py

--- src/pyesb_amqp/models.py
+++ src/pyesb_amqp/models.py
@@ -1,12 +1,27 @@
 from uuid import UUID
 
-from pydantic import BaseModel, Field, NonNegativeInt
+from pydantic import BaseModel, NonNegativeInt, field_validator
 
 
 class E1CMessage(BaseModel):
+    class Properties(BaseModel, extra="allow"):
+        integ_message_id: UUID
+        integ_message_correlation_id: UUID
+        SenderCode: str
+        RecipientCode: list[str]
+        integ_sender_code: str
+        integ_recipient_code: list[str]
+        integ_message_body_size: NonNegativeInt
+
     id: UUID
-    delivery_tag: NonNegativeInt = Field(validation_alias="delivery_number")
+    delivery_tag: bytes
+    delivery_number: NonNegativeInt
     durable: bool
     priority: NonNegativeInt
-    properties: dict[str, str]
+    properties: Properties
     body: bytes
+
+    @field_validator("delivery_tag", mode="before")
+    @classmethod
+    def delivery_tag_validator(cls, v: str) -> bytes:
+        return bytes.fromhex(v)
