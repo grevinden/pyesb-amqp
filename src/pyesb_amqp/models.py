77983327501b7423cd715ab88ba5c56a28d3a5f2
Anton Rastyazhenko <rastyazhenko.anton@gmail.com>
2026-06-23 06:23:19 +0300

    commit

--- src/pyesb_amqp/models.py
+++ src/pyesb_amqp/models.py
@@ -1,11 +1,11 @@
 from uuid import UUID
 
-from pydantic import BaseModel, NonNegativeInt
+from pydantic import BaseModel, Field, NonNegativeInt
 
 
 class E1CMessage(BaseModel):
     id: UUID
-    delivery_tag: str
+    delivery_tag: NonNegativeInt = Field(validation_alias="delivery_number")
     durable: bool
     priority: NonNegativeInt
     properties: dict[str, str]
